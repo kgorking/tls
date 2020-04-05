a92a4e79ba323ef2d06e602d431bdb49f1215033
Kenneth Gorking <dasapfe@yahoo.dk>
2020-04-05 14:07:58 +0200

    Updated example

--- examples/replicator/replicator.cpp
+++ examples/replicator/replicator.cpp
@@ -8,20 +8,25 @@
 using namespace std::chrono_literals;
 
 void send_value(tls::replicator<int>& dest, int value) {
-    std::cout << "\nmain: sending value " << value << '\n';
+    std::cout << "\nmain: sending value " << value << std::endl;
     dest.write(value);
-    std::this_thread::sleep_for(1s);
+    std::this_thread::sleep_for(250ms);
 }
 
 int main() {
-    tls::replicator<int> repl{ 1 };
+    // A mutex to prevent printing from being messed up
     std::mutex cout_mtx;
 
+    // The replicator
+    tls::replicator<int> repl{ 1 };
+
     // The reader lambda to run on threads
-    auto const reader = [&repl, &cout_mtx](int thread_index, int start_val) {
+    auto const reader = [&repl, &cout_mtx](int const thread_index, int const start_val) {
         int last_read = start_val;
+        int num_reads = 0;
         while (true) {
             int const val = repl.read();
+            num_reads++;
 
             if (val == -1) {
                 std::scoped_lock sl{ cout_mtx };
@@ -33,17 +38,19 @@ int main() {
                 last_read = val;
 
                 std::scoped_lock sl{ cout_mtx };
-                std::cout << thread_index << ": got new value " << val << '\n';
+                std::cout << thread_index << ": got new value '" << val << "', read old value " << num_reads << " times\n";
+                num_reads = 0;
             }
 
-            std::this_thread::sleep_for(20ms);
+            std::this_thread::sleep_for(5ms);
+            //std::this_thread::yield(); // will peg the cpu at 100% utilization
         }
     };
 
     // Fire up some threads
-    constexpr int num_threads = 32;
+    int const num_threads = std::thread::hardware_concurrency();
     std::vector<std::thread> threads;
-    for (int i = 0; i < num_threads; i++)
+    for (int i = 1; i <= num_threads; i++)
         threads.emplace_back(reader, i, repl.read());
 
     // send some values

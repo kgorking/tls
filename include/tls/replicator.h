fe7e2c62fda33fb848689828b63d5ddf4c239362
Kenneth Gorking <dasapfe@yahoo.dk>
2020-04-05 13:34:28 +0200

    Updated comments and naming

--- include/tls/replicator.h
+++ include/tls/replicator.h
@@ -5,9 +5,19 @@
 #include <vector>
 
 namespace tls {
+    /// <summary>
+    /// This class can be used to replicate data from one writer thread to other reader threads.
+    /// Data written to the replicator is copied to all threads that read
+    /// from it, with minimal use of locking.
+    ///
+    /// The writer thread locks when data is written. Reader threads only lock when 
+    /// trying to read modified data, when it's not modified it reads from its local copy.
+    /// </summary>
+    /// <typeparam name="T">The type to replicate to other threads</typeparam>
     template <class T>
     class replicator {
-        struct instance_data {
+        // Holds the threads local copy of an instances data
+        struct local_instance_copy {
             replicator<T>* owner{ nullptr };
             T data_copy;
             bool invalidated{ };
@@ -15,21 +25,18 @@ namespace tls {
 
         // This struct manages the per-thread data
         struct thread_data {
-            using iterator = typename std::vector<instance_data>::iterator;
+            using iterator = typename std::vector<local_instance_copy>::iterator;
 
-            thread_data(replicator<T>& instance, T const& t) {
-                set_data(instance, t);
-            }
+            // The thread is dead, so remove references to it from all instances
             ~thread_data() {
-                // Remove this instance_data from any replicator<> instances that has accessed it
-                for (instance_data& inst : instances) {
+                for (local_instance_copy& inst : instances) {
                     if (nullptr != inst.owner) {
                         inst.owner->remove_thread(*this);
                     }
                 }
             }
 
-            // Find the instance iterator. It needs the caller instance for internal bookkeeping
+            // Find the instance, returns an iterator. It needs the caller instance for internal bookkeeping
             iterator find(replicator<T>& instance) {
                 // Find the thread_local_data and return its local value
                 auto const it = std::find_if(instances.begin(), instances.end(),
@@ -38,9 +45,9 @@ namespace tls {
                     return it;
                 }
 
-                // No instance_data was found, so this is the first time it has been accessed (ie. new thread)
+                // No local_instance_copy was found, so this is the first time it has been accessed (ie. new thread)
                 instance.init_thread(*this);
-                instance_data id{ &instance, instance.base_data(), false };
+                local_instance_copy id{ &instance, instance.base_data(), false };
                 instances.push_back(id);
                 return instances.end() - 1;
             }
@@ -51,13 +58,14 @@ namespace tls {
                 it->invalidated = true;
             }
 
-            // Returns a copy of the local data
+            // Returns the local data
             T const& read(replicator<T>& instance) {
                 auto const it = find(instance);
                 update_data(*it);
                 return it->data_copy;
             }
 
+            // Pass the local data to a user function
             template<class Fn>
             auto read(replicator<T>& instance, Fn f) {
                 auto const it = find(instance);
@@ -68,28 +76,28 @@ namespace tls {
                 return f(const_data);
             }
 
-            // Sets the local data
+            // Sets a copy of the data for an instance
             void set_data(replicator<T>& instance, T const& t) {
                 auto const it = find(instance);
                 it->data_copy = t;
             }
 
-            // Resets this instances owner to null
+            // Resets the instances owner to null
             void reset_owner(replicator<T>& instance) {
                 auto const it = find(instance);
                 it->owner = nullptr;
             }
 
+            // Removes an instance from the thread
             void remove(replicator<T>& instance) {
-                //instances.remove_if([instance](auto& acc) { return instance == acc.owner; });
-                auto end = std::remove_if(instances.begin(), instances.end(),
-                                          [instance](auto& acc) { return instance == acc.owner; });
+                auto const end = std::remove_if(instances.begin(), instances.end(),
+                                                [instance](auto& acc) { return instance == acc.owner; });
                 instances.erase(end, instance.end());
             }
 
         private:
             // Check to see if the data needs updating
-            void update_data(instance_data& id) {
+            void update_data(local_instance_copy& id) {
                 if (id.invalidated) {
                     id.invalidated = false;
 
@@ -98,11 +106,11 @@ namespace tls {
                 }
             }
 
-            std::vector<instance_data> instances{};
+            std::vector<local_instance_copy> instances{};
         };
         friend thread_data;
 
-        // Store a thread_data and set its data.
+        // Store a thread_data in this instance
         void init_thread(thread_data& t) {
             std::unique_lock ul(mtx);
 
@@ -117,9 +125,9 @@ namespace tls {
             threads.erase(it);
         }
 
-        // Get the thread-local instance
+        // Get the thread_data instance for the current thread
         thread_data& local() {
-            thread_local thread_data inst{ *this, data };
+            thread_local thread_data inst{};
             return inst;
         }
 
@@ -128,21 +136,24 @@ namespace tls {
         replicator(T const& t) : data(t) {}
 
         ~replicator() {
-            for (auto inst : threads)
-                inst->reset_owner(*this);
+            for (auto thr_data : threads)
+                thr_data->reset_owner(*this);
         }
 
+        // Read the threads copy of data
         T const& read() {
             return local().read(*this);
         }
 
+        // Pass the threads copy of data to a user function
         template<class Fn>
         auto read(Fn f) {
             return local().read(*this, f);
         }
 
+        // Set the master data
         void write(T const& t) {
-            // Take a unique lock on the data so no other thread can get to it
+            // Take a unique lock on the data so no other thread can modify it
             std::unique_lock ul(mtx);
 
             // Write the data
@@ -153,18 +164,19 @@ namespace tls {
                 thread->invalidate(*this);
         }
 
+        // Get the master data
         T const& base_data() const {
             return data;
         }
 
     private:
-        // the threads that access this instance
+        // the threads that access this instance. Changes to 'data' will be replicated to these threads
         std::vector<thread_data*> threads;
 
         // The central data that is replicated to threads
         T data;
 
-        // Mutex to serialize access to the data when it is modified
+        // Mutex to serialize access to 'data' when it is modified
         std::shared_mutex mtx;
     };
 }

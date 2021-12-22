d6c09b25f90d0a83556a40ba213e608c521d3e01
Kenneth Gorking <kgorking@outlook.com>
2021-12-22 16:07:25 +0100

    tls::split is now useable in constexpr contexts

--- include/tls/split.h
+++ include/tls/split.h
@@ -19,14 +19,14 @@ class split {
 	struct thread_data {
 		// The destructor triggers when a thread dies and the thread_local
 		// instance is destroyed
-		~thread_data() noexcept {
+		constexpr ~thread_data() noexcept {
 			if (owner != nullptr) {
 				owner->remove_thread(this);
 			}
 		}
 
 		// Return a reference to an instances local data
-		T &get(split *instance) noexcept {
+		[[nodiscard]] constexpr T& get(split* instance) noexcept {
 			// If the owner is null, (re-)initialize the instance.
 			// Data may still be present if the thread_local instance is still active
 			if (owner == nullptr) {
@@ -37,7 +37,7 @@ class split {
 			return data;
 		}
 
-		void remove(split *instance) noexcept {
+		constexpr void remove(split* instance) noexcept {
 			if (owner == instance) {
 				data = {};
 				owner = nullptr;
@@ -45,20 +45,20 @@ class split {
 			}
 		}
 
-		T *get_data() noexcept {
+		[[nodiscard]] constexpr T* get_data() noexcept {
 			return &data;
 		}
-		T const *get_data() const noexcept {
+		[[nodiscard]] constexpr T const* get_data() const noexcept {
 			return &data;
 		}
 
-		void set_next(thread_data *ia) noexcept {
+		constexpr void set_next(thread_data* ia) noexcept {
 			next = ia;
 		}
-		thread_data *get_next() noexcept {
+		[[nodiscard]] constexpr thread_data* get_next() noexcept {
 			return next;
 		}
-		thread_data const *get_next() const noexcept {
+		[[nodiscard]] constexpr thread_data const* get_next() const noexcept {
 			return next;
 		}
 
@@ -73,74 +73,122 @@ private:
 	thread_data *head{};
 
 	// Mutex for serializing access for adding/removing thread-local instances
-	std::mutex mtx_storage;
+	std::mutex* mtx_storage{};
+
+	// Data that is only used in constexpr evaluations
+	thread_data consteval_data{};
 
 protected:
-	// Adds a thread_data
-	void init_thread(thread_data *t) noexcept {
-		std::scoped_lock sl(mtx_storage);
+	[[nodiscard]] std::mutex& get_runtime_mutex() noexcept {
+		return *mtx_storage;
+	}
 
-		t->set_next(head);
-		head = t;
+	// Adds a thread_data
+	constexpr void init_thread(thread_data* t) noexcept {
+		auto const init_thread_imp = [&]() noexcept {
+			t->set_next(head);
+			head = t;
+		};
+
+		if (!std::is_constant_evaluated()) {
+			std::scoped_lock sl(get_runtime_mutex());
+			init_thread_imp();
+		} else {
+			init_thread_imp();
+		}
 	}
 
 	// Remove the thread_data
-	void remove_thread(thread_data *t) noexcept {
-		std::scoped_lock sl(mtx_storage);
-
-		// Remove the thread from the linked list
-		if (head == t) {
-			head = t->get_next();
-		} else {
-			auto curr = head;
-			while (curr->get_next() != nullptr) {
-				if (curr->get_next() == t) {
-					curr->set_next(t->get_next());
-					return;
-				} else {
-					curr = curr->get_next();
+	constexpr void remove_thread(thread_data* t) noexcept {
+		auto const remove_thread_impl = [&]() noexcept {
+			// Remove the thread from the linked list
+			if (head == t) {
+				head = t->get_next();
+			} else {
+				auto curr = head;
+				while (curr->get_next() != nullptr) {
+					if (curr->get_next() == t) {
+						curr->set_next(t->get_next());
+						return;
+					} else {
+						curr = curr->get_next();
+					}
 				}
 			}
+		};
+
+		if (!std::is_constant_evaluated()) {
+			std::scoped_lock sl(get_runtime_mutex());
+			remove_thread_impl();
+		} else {
+			remove_thread_impl();
 		}
 	}
 
 public:
-	split() noexcept = default;
-	split(split const &) = delete;
-	split(split &&) noexcept = default;
-	split &operator=(split const &) = delete;
-	split &operator=(split &&) noexcept = default;
-	~split() noexcept {
-		clear();
+	constexpr split() noexcept {
+		if (!std::is_constant_evaluated())
+			mtx_storage = new std::mutex;
+	}
+	constexpr split(split const&) = delete;
+	constexpr split(split&&) noexcept = default;
+	constexpr split& operator=(split const&) = delete;
+	constexpr split& operator=(split&&) noexcept = default;
+	constexpr ~split() noexcept {
+		reset();
+
+		if (!std::is_constant_evaluated())
+			delete mtx_storage;
 	}
 
 	// Get the thread-local instance of T
-	T &local() noexcept {
-		thread_local thread_data var{};
-		return var.get(this);
+	constexpr T& local() noexcept {
+		if (!std::is_constant_evaluated()) {
+			auto const local_impl = [&]() -> T& {
+				thread_local thread_data var{};
+				return var.get(this);
+			};
+			return local_impl();
+		} else {
+			return consteval_data.get(this);
+		}
 	}
 
 	// Performa an action on all each instance of the data
 	template<class Fn>
-	void for_each(Fn&& fn) {
-		std::scoped_lock sl(mtx_storage);
+	constexpr void for_each(Fn&& fn) {
+		auto const for_each_impl = [&]() noexcept {
+			for (thread_data* thread = head; thread != nullptr; thread = thread->get_next()) {
+				fn(*thread->get_data());
+			}
+		};
 
-		for (thread_data *instance = head; instance != nullptr; instance = instance->get_next()) {
-			fn(*instance->get_data());
+		if (!std::is_constant_evaluated()) {
+			std::scoped_lock sl(get_runtime_mutex());
+			for_each_impl();
+		} else {
+			for_each_impl();
 		}
 	}
 
-	// Clears all data and threads
-	void clear() noexcept {
-		std::scoped_lock sl(mtx_storage);
+	// Resets all data and threads
+	constexpr void reset() noexcept {
+		auto const impl = [&] {
+			for (thread_data* instance = head; instance != nullptr;) {
+				auto next = instance->get_next();
+				instance->remove(this);
+				instance = next;
+			}
 
-		for (thread_data *instance = head; instance != nullptr;) {
-			auto next = instance->get_next();
-			instance->remove(this);
-			instance = next;
+			head = nullptr;
+		};
+
+		if (!std::is_constant_evaluated()) {
+			std::scoped_lock sl(get_runtime_mutex());
+			impl();
+		} else {
+			impl();
 		}
-
-		head = nullptr;
 	}
 };
 } // namespace tls

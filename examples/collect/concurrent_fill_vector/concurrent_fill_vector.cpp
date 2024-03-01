9890afbfb928bbbc6d34367fbe0203b1cd64ad4f
Kenneth Gorking <kgorking@outlook.com>
2024-03-01 09:49:00 +0100

    Updated example

--- examples/collect/concurrent_fill_vector/concurrent_fill_vector.cpp
+++ examples/collect/concurrent_fill_vector/concurrent_fill_vector.cpp
@@ -13,21 +13,12 @@ void dump(std::vector<unsigned> const& v) {
 }
 
 // Prints the threaded vectors with a '-' to indicate threaded splits
-void dump_threaded(std::vector<std::vector<unsigned>> const& threaded_vec) {
-    for (auto const& vec : threaded_vec) {
-        dump(vec);
-        std::cout << "- ";
-    }
-    std::cout << '\n' << '\n';
-}
-
-// Merges all the threaded vectors in to a single vector
-std::vector<unsigned> flatten(std::vector<std::vector<unsigned>> const& threaded_vec) {
-	std::vector<unsigned> vec;
-    for (auto const& t_vec : threaded_vec) {
-        vec.insert(vec.end(), t_vec.begin(), t_vec.end());
-    }
-    return vec;
+template<typename T>
+void dump_threaded(tls::collect<T> const& collector) {
+	collector.for_each([](auto const& vec) {
+		dump(vec);
+		std::cout << "- ";
+	});
 }
 
 int main() {
@@ -41,38 +32,44 @@ int main() {
     // Vector to concurrently iterate over
 	std::vector<unsigned> input(num_inputs);
     std::iota(input.begin(), input.end(), 0);
-    std::cout << "Initial data:\n";
-    dump(input);
-    std::cout << '\n' << '\n';
 
-    // Run some concurrent code that would normally create a data race
+	// The thread-local vector
 	tls::collect<std::vector<unsigned>> vec;
+
+	// Create a worker that writes into a thread-local vector
 	auto const worker = [&](unsigned start) {
         auto& local = vec.local();
 		for (unsigned i = start; i < start + chunk_size; i++)
             local.push_back(i);
     };
 
-    std::vector<std::thread> threads;
-	for (unsigned thr = 0; thr < num_threads; thr++) {
-        threads.emplace_back(worker, thr * chunk_size);
-    }
-
-    for (auto &thread : threads) {
-		thread.join();
+    // Run some concurrent code that would normally create a data race
+	{
+		std::vector<std::jthread> threads;
+		for (unsigned thr = 0; thr < num_threads; thr++) {
+			threads.emplace_back(worker, thr * chunk_size);
+		}
 	}
 
+	// Dump some data for verification
+	std::cout << "Initial data:\n";
+		dump(input);
+    std::cout << '\n' << '\n';
+
     std::cout << "Concurrent push_back result:\n";
-	auto const collect = vec.gather();
-    dump_threaded(collect);
+	    dump_threaded(vec);
+	std::cout << '\n' << '\n';
 
     std::cout << "Flattened:\n";
-	std::vector<unsigned> reduced_vec = flatten(collect);
-    dump(reduced_vec);
+		std::vector<unsigned> reduced_vec;
+		vec.gather_flattened(std::back_inserter(reduced_vec));
+		dump(reduced_vec);
     std::cout << '\n' << '\n';
 
     std::cout << "Sorted:\n";
-	std::sort(reduced_vec.begin(), reduced_vec.end());
-    dump(reduced_vec);
+		std::sort(reduced_vec.begin(), reduced_vec.end());
+		dump(reduced_vec);
     std::cout << '\n' << '\n';
+
+	std::cout << "Matches initial data? " << std::boolalpha << (input == reduced_vec) << '\n';
 }
